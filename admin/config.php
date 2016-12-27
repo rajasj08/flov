cfc656cade7457509adb3621c0890f329e0ccbca
rajasj08 <rajasj08@gmail.com>
2016-12-27 12:20:36 +0530

    second commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oldversion-env.ap-south-1.elasticbeanstalk.com/admin/');
+define('HTTP_CATALOG', 'http://oldversion-env.ap-south-1.elasticbeanstalk.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/GitHub/floldversion/admin/');
+define('HTTPS_CATALOG', 'http://localhost/GitHub/floldversion/');
+
+// DIR
+define('DIR_APPLICATION', '/var/app/current/admin/');
+define('DIR_SYSTEM', '/var/app/current/system/');
+define('DIR_DATABASE', '/var/app/current/system/database/');
+define('DIR_LANGUAGE', '/var/app/current/admin/language/');
+define('DIR_TEMPLATE', '/var/app/current/admin/view/template/');
+define('DIR_CONFIG', '/var/app/current/system/config/');
+define('DIR_IMAGE', 'https://s3.ap-south-1.amazonaws.com/floldversion/image/');
+define('DIR_CACHE', '/var/app/current/system/cache/');
+define('DIR_DOWNLOAD', '/var/app/current/download/');
+define('DIR_LOGS', '/var/app/current/system/logs/');
+define('DIR_CATALOG', '/var/app/current/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'aa1cm8bqx9c8wzy.cvwrkeif9dtm.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'fladmin');
+define('DB_PASSWORD', 'Welcome!23');
+define('DB_DATABASE', 'ebdb');
+define('DB_PREFIX', 'oc_');
+?>
